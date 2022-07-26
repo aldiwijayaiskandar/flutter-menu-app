622fe9a02c24b13b5080bf8be3284578e332f5d8
Aldi <aldi.app@qubisa.com>
2022-07-26 09:22:00 +0700

    [main] fixing menu impl

--- test/data/repos/menu_impl_test.dart
+++ test/data/repos/menu_impl_test.dart
@@ -6,7 +6,7 @@ import 'package:test_saham_rakyat/data/repos/menu_impl.dart';
 import 'package:test_saham_rakyat/data/services/services.dart';
 import 'package:test_saham_rakyat/domain/entities/menu.dart';
 
-class MockDioApi extends Mock implements DioApi {}
+import '../../utils/dio_api_mock.dart';
 
 void main() {
   final DioApi dioApi = MockDioApi();
@@ -34,6 +34,7 @@ void main() {
   List<Menu> expectedMenus = List<Map<String, dynamic>>.from(meals)
       .map((json) => MenuModel.fromJson(json).toEntity())
       .toList();
+
   test("Getting Menus", () async {
     when(() => dioApi.getApi(url: "/v1/1/filter.php?c=Beef"))
         .thenAnswer((_) => menus);
@@ -41,8 +42,10 @@ void main() {
     // act
     final result = await repo.getMenus("Beef");
 
-    expect(result[0].id == expectedMenus[0].id, true);
-    expect(result[0].name == expectedMenus[0].name, true);
-    expect(result[0].thumbnail == expectedMenus[0].thumbnail, true);
+    for (int i = 0; i < result.length; i++) {
+      expect(result[i].id, expectedMenus[i].id);
+      expect(result[i].name, expectedMenus[i].name);
+      expect(result[i].thumbnail, expectedMenus[i].thumbnail);
+    }
   });
 }
